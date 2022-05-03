886476df28a4549483b39960e530420d485700a6
Jermaine <jmainejj1997@gmail.com>
2022-05-03 19:44:39 -0400

    changed board display into grid instead of flex

--- main.js
+++ main.js
@@ -3,20 +3,42 @@ let mainDiv = document.createElement('div')
 mainDiv.className = "main-div"
 document.body.append(mainDiv)
 
-function board(){
-    for (let i = 1; i < 17; i++){
-        // console.log(i)
-        let col = document.createElement('div')
-        col.className = 'col'
-        col.textContent = i
-        mainDiv.append(col)
+let buttonDiv = document.createElement('div')
+buttonDiv.className = 'buttonDiv'
+let erase = document.createElement('button')
+erase.textContent = 'erase'
+let black = document.createElement('button')
+black.textContent = 'black'
+let blue = document.createElement('button')
+blue.textContent = 'blue'
+let reset = document.createElement('button')
+reset.textContent = 'reset'
 
-        for (let j = 1; j < 17; j++){
-            console.log(j)
-            let row = document.createElement('div')
-            row.className = 'row'
-            col.append(row)
-        }
+erase.className = "erase"
+black.className = "black-div"
+blue.className = "blue-div"
+reset.className = "reset-div"
+
+let num = 16
+
+function board(){
+    for (let i = 0; i < num**2; i++){
+        let block = document.createElement('div')
+        block.className = 'block'
+        block.textContent = i
+        mainDiv.append(block)
+
+        // let block = document.createElement('div')
+        // block.className = 'block'
+        // // block.style.grid
+        // block.textContent = i
+        // container.append(block)
     }
 }
 board()
+
+// document.body.append(buttonDiv)
+// buttonDiv.append(erase)
+// buttonDiv.append(black)
+// buttonDiv.append(blue)
+// buttonDiv.append(reset)
